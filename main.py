6244c459b551bdd51f15ceaffbb25614c50d3098
Tushan-45 <tushansinha01@gmail.com>
2026-06-01 19:48:10 +0530

    railway port fix

--- main.py
+++ main.py
@@ -88,6 +88,6 @@ def get_uploaded_file(filename):
         filename
     )
 
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
